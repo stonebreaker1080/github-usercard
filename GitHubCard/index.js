2020a1c0196a3e93259793619a9bde7f0c3a97dc
stonebreaker1080 <stonebreaker1080@protonmail.com>
2022-02-10 18:56:55 -0500

    submission

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -1,8 +1,42 @@
+import axios from "axios";
 /*
   STEP 1: using axios, send a GET request to the following URL
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
+
+const username = "stonebreaker1080"
+
+const friendsArray = [
+  "stonebreaker1080",
+  "tetondan",
+  "dustinmyers",
+  "justsml",
+  "luishrd",
+  "bigknell"
+]
+
+const getProfile = (username) => {
+  axios.get(`https://api.github.com/users/${username}`)
+  .then(resp => {
+    console.log(`getting api response: `, resp.data)
+    const profileCard = createProfileCard(resp.data)
+    document.querySelector(".cards").appendChild(profileCard)
+  })
+  .catch(err => {
+    console.error(err)
+  })
+  .finally(() => console.log("done"))
+}
+
+const getProfiles = (inputArray) => {
+  inputArray.forEach(item =>
+    getProfile(item))
+}
+
+
+// getProfile(username)
+getProfiles(friendsArray)
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -16,6 +50,10 @@
   STEP 4: Pass the data received from Github into your function,
     and append the returned markup to the DOM as a child of .cards
 */
+
+
+
+
 
 /*
   STEP 5: Now that you have your own card getting added to the DOM, either
@@ -49,6 +87,67 @@ const followersArray = [];
       </div>
     </div>
 */
+
+
+function createProfileCard(profileObject) {
+  
+
+  let divCard = document.createElement("div")
+  let cardImage = document.createElement("img")
+  let cardInfo = document.createElement("div")
+  let h3CardName = document.createElement("h3")
+  let pUsername = document.createElement("p")
+  let pLocation = document.createElement("p")
+  let pProfile = document.createElement("p")
+  let aProfile = document.createElement("a")
+  let pFollowers = document.createElement("p")
+  let pFollowing = document.createElement("p")
+  let pBio = document.createElement("p")
+
+  divCard.classList.add("card")
+  cardImage.src = profileObject.avatar_url
+  cardInfo.classList.add("card-info")
+  h3CardName.classList.add("name")
+  h3CardName.textContent = profileObject.name
+  pUsername.classList.add("username")
+  pUsername.textContent = profileObject.login
+  pLocation.textContent = `Location: ${profileObject.location}`
+  pProfile.textContent = "Profile: "
+  aProfile.href = profileObject.url
+  aProfile.textContent = profileObject.url
+  pFollowers.textContent = `Followers: ${profileObject.followers}`
+  pFollowing.textContent = `Following: ${profileObject.following}`
+  pBio.textContent = `Bio: ${profileObject.bio}`
+
+  divCard.appendChild(cardImage)
+  divCard.appendChild(cardInfo)
+  cardInfo.appendChild(h3CardName)
+  cardInfo.appendChild(pUsername)
+  cardInfo.appendChild(pLocation)
+  cardInfo.appendChild(pProfile)
+  pProfile.appendChild(aProfile)
+  cardInfo.appendChild(pFollowers)
+  cardInfo.appendChild(pFollowing)
+  cardInfo.appendChild(pBio)
+
+  return divCard
+}
+
+
+// <div class="card">
+//       <img src={image url of user} />
+//       <div class="card-info">
+//         <h3 class="name">{users name}</h3>
+//         <p class="username">{users user name}</p>
+//         <p>Location: {users location}</p>
+//         <p>Profile:
+//           <a href={address to users github page}>{address to users github page}</a>
+//         </p>
+//         <p>Followers: {users followers count}</p>
+//         <p>Following: {users following count}</p>
+//         <p>Bio: {users bio}</p>
+//       </div>
+//     </div>
 
 /*
   List of LS Instructors Github username's:
